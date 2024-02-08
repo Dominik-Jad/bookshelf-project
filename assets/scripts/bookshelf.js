8e7289bd93b6aa84cc143d153e30a70c5b8bd7b1
Gelisim Duygusu <gelisimduygusu@gmail.com>
2024-02-08 16:13:45 +0000

    Remove button added

--- assets/scripts/bookshelf.js
+++ assets/scripts/bookshelf.js
@@ -1,105 +1,109 @@
 $(document).ready(function () {
 
-// add books from local storage to the to-read container
-function displayBooks() {
-    var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
-    for (var i = 0; i < savedBooks.length; i++) {
-        if (savedBooks[i].read) {
-            continue;
+    // Function to display books from local storage
+    function displayBooks() {
+        var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
+        $("#to-read").empty();
+        for (var i = 0; i < savedBooks.length; i++) {
+            if (!savedBooks[i].read) {
+                var container = $("<div>").addClass("col-lg-6 col-md-12 col-sm-12");
+                var bookCard = $("<div>").addClass("card position-relative");
+                var bookBody = $("<div>").addClass("card-body");
+                var bookTitle = $("<h5>").addClass("card-title").text(savedBooks[i].title);
+                var bookAuthor = $("<p>").addClass("card-text").text(savedBooks[i].author);
+                var bookImage = $("<img>").addClass("card-img-top").attr("src", savedBooks[i].image);
+                var bookLink = $("<a>").addClass("btn btn-primary").attr("href", savedBooks[i].link).text("View");
+                var readBtn = $("<button>").addClass("btn btn-danger read-it-btn").text("Read it!").attr("data-id", i);
+                var removeBtn = $("<button>").addClass("btn btn-outline-secondary remove-btn position-absolute top-0 start-0 mt-2 ms-2 p-0").html('<i class="fas fa-times"></i>').attr("data-id", i);
+                bookBody.append(bookTitle, bookAuthor, bookImage, bookLink, readBtn);
+                bookCard.append(bookBody, removeBtn);
+                container.append(bookCard);
+                $("#to-read").append(container);
+            }
         }
-        var book = savedBooks[i];
-        var container = $("<div>").addClass("col-lg-6 col-md-12 col-sm-12");
-        var bookCard = $("<div>").addClass("card");
-        var bookBody = $("<div>").addClass("card-body");
-        var bookTitle = $("<h5>").addClass("card-title").text(book.title);
-        var bookAuthor = $("<p>").addClass("card-text").text(book.author);
-        var bookImage = $("<img>").addClass("card-img-top").attr("src", book.image);
-        var bookLink = $("<a>").addClass("btn btn-primary").attr("href", book.link).text("View");
-        var readBtn = $("<button>").addClass("btn btn-danger read-it-btn").text("Read it!").attr("data-id", i);
-        bookBody.append(bookTitle, bookAuthor, bookImage);
-        bookCard.append(bookBody,bookLink, readBtn);
-        container.append(bookCard);
-       $("#to-read").append(container);
     }
-}
 
-// display fave books from local storage into the fave section
-function displayFaveBooks() {
-    var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
-    for (var i = 0; i < savedBooks.length; i++) {
-        var book = savedBooks[i];
-        if (book.fave) {
-            var container = $("<div>").addClass("col-lg-6 col-md-12 col-sm-12");
-            var bookCard = $("<div>").addClass("card");
-            var bookBody = $("<div>").addClass("card-body");
-            var bookTitle = $("<h5>").addClass("card-title").text(book.title);
-            var bookAuthor = $("<p>").addClass("card-text").text(book.author);
-            var bookImage = $("<img>").addClass("card-img-top").attr("src", book.image);
-            var bookLink = $("<a>").addClass("btn btn-primary").attr("href", book.link).text("View");
-            bookBody.append(bookTitle, bookAuthor, bookImage );
-            bookCard.append(bookBody, bookLink);
-            container.append(bookCard);
-            $("#favourites").append(container);
+    // Function to display favorite books from local storage
+    function displayFaveBooks() {
+        var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
+        $("#favourites").empty();
+        for (var i = 0; i < savedBooks.length; i++) {
+            if (savedBooks[i].fave) {
+                var container = $("<div>").addClass("col-lg-6 col-md-12 col-sm-12");
+                var bookCard = $("<div>").addClass("card position-relative");
+                var bookBody = $("<div>").addClass("card-body");
+                var bookTitle = $("<h5>").addClass("card-title").text(savedBooks[i].title);
+                var bookAuthor = $("<p>").addClass("card-text").text(savedBooks[i].author);
+                var bookImage = $("<img>").addClass("card-img-top").attr("src", savedBooks[i].image);
+                var bookLink = $("<a>").addClass("btn btn-primary").attr("href", savedBooks[i].link).text("View");
+                var removeBtn = $("<button>").addClass("btn btn-outline-secondary remove-btn position-absolute top-0 start-0 mt-2 ms-2 p-0").html('<i class="fas fa-times"></i>').attr("data-id", i);
+                bookBody.append(bookTitle, bookAuthor, bookImage, bookLink);
+                bookCard.append(bookBody, removeBtn);
+                container.append(bookCard);
+                $("#favourites").append(container);
+            }
         }
     }
-}
-// display books from local storage into the read section
-function displayReadBooks() {
-    var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
-    for (var i = 0; i < savedBooks.length; i++) {
-        var book = savedBooks[i];
-        if(book.fave){
-            continue;
-        }
-        if (book.read) {
-            var container = $("<div>").addClass("col-lg-6 col-md-12 col-sm-12");
-            var bookCard = $("<div>").addClass("card");
-            var bookBody = $("<div>").addClass("card-body");
-            var bookTitle = $("<h5>").addClass("card-title").text(book.title);
-            var bookAuthor = $("<p>").addClass("card-text").text(book.author);
-            var bookImage = $("<img>").addClass("card-img-top").attr("src", book.image);
-            var bookLink = $("<a>").addClass("btn btn-primary").attr("href", book.link).text("View");
-            var faveBtn = $("<button>").addClass("btn btn-warning fave-btn").text("Favorite").attr("data-id", i);
-            bookBody.append(bookTitle, bookAuthor, bookImage);
-            bookCard.append(bookBody, bookLink, faveBtn);
-            container.append(bookCard);
-            $("#have-read").append(container);
+
+    // Function to display read books from local storage
+    function displayReadBooks() {
+        var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
+        $("#have-read").empty();
+        for (var i = 0; i < savedBooks.length; i++) {
+            if (savedBooks[i].read && !savedBooks[i].fave) {
+                var container = $("<div>").addClass("col-lg-6 col-md-12 col-sm-12");
+                var bookCard = $("<div>").addClass("card position-relative");
+                var bookBody = $("<div>").addClass("card-body");
+                var bookTitle = $("<h5>").addClass("card-title").text(savedBooks[i].title);
+                var bookAuthor = $("<p>").addClass("card-text").text(savedBooks[i].author);
+                var bookImage = $("<img>").addClass("card-img-top").attr("src", savedBooks[i].image);
+                var bookLink = $("<a>").addClass("btn btn-primary").attr("href", savedBooks[i].link).text("View");
+                var favBtn = $("<button>").addClass("btn btn-warning fave-btn").text("Favorite").attr("data-id", i);
+                var removeBtn = $("<button>").addClass("btn btn-outline-secondary remove-btn position-absolute top-0 start-0 mt-2 ms-2 p-0").html('<i class="fas fa-times"></i>').attr("data-id", i);
+                bookBody.append(bookTitle, bookAuthor, bookImage, bookLink, favBtn);
+                bookCard.append(bookBody, removeBtn);
+                container.append(bookCard);
+                $("#have-read").append(container);
+            }
         }
     }
-}
-// event listener for read button
-$(document).on("click", ".read-it-btn", function (event) {
-    // add a new property to the book object to indicate that the book has been read
-    event.preventDefault();
-    var bookIndex = $(this).attr("data-id");
-    var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
-    savedBooks[bookIndex].read = true;
-    localStorage.setItem("myBooks", JSON.stringify(savedBooks));
-    console.log(savedBooks);
-    // clear the to-read container
-    $("#have-read").empty();
-    displayReadBooks();
-    $("#to-read").empty();
+
+    // Event listener for read button
+    $(document).on("click", ".read-it-btn", function (event) {
+        event.preventDefault();
+        var bookIndex = $(this).attr("data-id");
+        var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
+        savedBooks[bookIndex].read = true;
+        localStorage.setItem("myBooks", JSON.stringify(savedBooks));
+        displayReadBooks();
+        displayBooks();
+    });
+
+    // Event listener for favorite button
+    $(document).on("click", ".fave-btn", function (event) {
+        event.preventDefault();
+        var bookIndex = $(this).attr("data-id");
+        var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
+        savedBooks[bookIndex].fave = true;
+        localStorage.setItem("myBooks", JSON.stringify(savedBooks));
+        displayFaveBooks();
+        displayReadBooks();
+    });
+
+    // Event listener for remove button
+    $(document).on("click", ".remove-btn", function (event) {
+        event.preventDefault();
+        var bookIndex = $(this).attr("data-id");
+        var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
+        savedBooks.splice(bookIndex, 1);
+        localStorage.setItem("myBooks", JSON.stringify(savedBooks));
+        displayFaveBooks();
+        displayReadBooks();
+        displayBooks();
+    });
+
+    // Initial display of books
     displayBooks();
-});
-
-// add event listener for fave button
-$(document).on("click", ".fave-btn", function (event) {
-    event.preventDefault();
-    var bookIndex = $(this).attr("data-id");
-    var savedBooks = JSON.parse(localStorage.getItem("myBooks")) || [];
-    savedBooks[bookIndex].fave = true;
-    localStorage.setItem("myBooks", JSON.stringify(savedBooks));
-    console.log(savedBooks);
-    // clear the to-read container
-    $("#favourites").empty();
     displayFaveBooks();
-    $("#have-read").empty();
     displayReadBooks();
-});
-
-
-displayFaveBooks();
-displayReadBooks();
-displayBooks();
 });
